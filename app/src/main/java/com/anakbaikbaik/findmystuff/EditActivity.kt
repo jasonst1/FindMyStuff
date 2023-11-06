3b99db24b66ec1cfda8cf6fc2f5e196837354b0e
Jason Sebastian Tjoang <jasonsebastian20529@gmail.com>
2023-11-06 15:13:43 +0700

    EditPage Layout

--- app/src/main/java/com/anakbaikbaik/findmystuff/EditActivity.kt
+++ app/src/main/java/com/anakbaikbaik/findmystuff/EditActivity.kt
@@ -3,11 +3,14 @@ package com.anakbaikbaik.findmystuff
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -17,9 +20,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.anakbaikbaik.findmystuff.ui.theme.FindMyStuffTheme
 import com.anakbaikbaik.findmystuff.ui.theme.PrimaryTextButton
 import com.anakbaikbaik.findmystuff.ui.theme.RedTextButton
@@ -42,10 +49,18 @@ class EditActivity : ComponentActivity() {
 
 @Composable
 fun editColumn(){
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
         var text by remember { mutableStateOf("") }
-
-
+        Image(
+            painter = painterResource(R.drawable.monyet),
+            contentDescription = null,
+            modifier = Modifier
+                .size(200.dp),
+            contentScale = ContentScale.Crop
+        )
         Text(
             text = "Name"
         )
