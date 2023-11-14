f78449a97b2e21d30fd8492d63c68073dc30463d
Rasyid113579 <rasyid1357911@gmail.com>
2023-11-14 16:01:45 +0700

    Bottom_Navigation

--- app/src/main/java/com/anakbaikbaik/findmystuff/Screens/HomeScreen.kt
+++ app/src/main/java/com/anakbaikbaik/findmystuff/Screens/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.anakbaikbaik.findmystuff.Screens
 
+import androidx.compose.material3.Icon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -14,15 +15,32 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Refresh
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -30,15 +48,52 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.anakbaikbaik.findmystuff.Navigation.Screen
 import com.anakbaikbaik.findmystuff.R
 import com.anakbaikbaik.findmystuff.ui.theme.PrimaryTextButton
 import com.anakbaikbaik.findmystuff.ui.theme.RedTextButton
 import com.anakbaikbaik.findmystuff.ui.theme.topBar
 import com.anakbaikbaik.findmystuff.ui.theme.warnaUMN
+import com.anakbaikbaik.findmystuff.Navigation.Screen
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasNews: Boolean,
+    val badgeCount: Int? = null
+)
 
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
 fun HomeScreen(navController: NavController) {
+    val items = listOf(
+        BottomNavigationItem(
+            title = "HomeScreen",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+            hasNews = false,
+        ),
+        BottomNavigationItem(
+            title = "AddScreen",
+            selectedIcon = Icons.Filled.Add,
+            unselectedIcon = Icons.Outlined.Add,
+            hasNews = false,
+        ),
+        BottomNavigationItem(
+            title = "EditScreen",
+            selectedIcon = Icons.Filled.Refresh,
+            unselectedIcon = Icons.Outlined.Refresh,
+            hasNews = false,
+        )
+    )
+    val screenMap = mapOf(
+        "HomeScreen" to Screen.HomeScreen,
+        "AddScreen" to Screen.AddScreen,
+        "EditScreen" to Screen.EditScreen
+    )
+    var selectedItemIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
@@ -49,18 +104,38 @@ fun HomeScreen(navController: NavController) {
                 // Add padding to the main content area
                 Conversation(SampleData.conversationSample, navController)
             },
-            floatingActionButton = {
-                FloatingActionButton(
-                    onClick = {navController.navigate(Screen.AddScreen.route)},
-                    modifier = Modifier // Adjust alignment as needed
-                        .padding(16.dp)
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.add),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(40.dp)
-                    )
+            bottomBar = {
+                NavigationBar() {
+                    items.forEachIndexed{ index, item ->
+                        NavigationBarItem(
+                            selected = selectedItemIndex == index,
+                            onClick = {
+                                selectedItemIndex = index
+                                screenMap[item.title]?.let { navController.navigate(it.route) }
+                            },
+                            alwaysShowLabel = false,
+                            icon = {
+                                BadgedBox(
+                                    badge = {
+                                        if(item.badgeCount != null) {
+                                            Badge {
+                                                Text(text = item.badgeCount.toString())
+                                            }
+                                        } else if(item.hasNews) {
+                                            Badge()
+                                        }
+                                    }
+                                ) {
+                                    Icon(
+                                        imageVector = if (index == selectedItemIndex) {
+                                            item.selectedIcon
+                                        } else item.unselectedIcon,
+                                        contentDescription = item.title
+                                    )
+                                }
+                            }
+                        )
+                    }
                 }
             }
         )
